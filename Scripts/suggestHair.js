9221b9b2bb0f520b257b6fb5de8ac974e0150436
Emma Hartnall | Hall <emmahartnall@gmail.com>
2024-09-28 00:28:46 +1000

    suggested styles displayed

--- Scripts/suggestHair.js
+++ Scripts/suggestHair.js
@@ -8,9 +8,27 @@ document.getElementById('customerHairForm').addEventListener('submit', function(
     var imageDisplay = document.getElementById('imageDisplay');
     imageDisplay.innerHTML = ''; // Clear previous images
 
-    var img = document.createElement('img');
-    img.src = 'images/people.png';
-    img.alt = 'Loading...';
-    imageDisplay.appendChild(img);
+    var img1 = document.createElement('img');
+    img1.src = "./Images/manBrown.jpg";
+    console.log('img1', img1);
+    imageDisplay.appendChild(img1);
+    console.log('imagedisplay', imageDisplay);
+    console.log('hairType', hairType);
+
+    var img2 = document.createElement('img');
+    img2.src = './Images/womanBlack.jpg';
+    imageDisplay.appendChild(img2);
+    //console.log('hairLength', hairLength);
+
+    var img3 = document.createElement('img');
+    img3.src = './Images/womanBlondShort.jpg';
+    imageDisplay.appendChild(img3);
+    //console.log('hairColour', hairColour);
+
+    var img4 = document.createElement('img');
+    img4.src = './Images/WomanBrownShort.jpg';
+    imageDisplay.appendChild(img4);
+    //console.log('gender', gender);
+    //alert('Thank you for your feedback!');
 
 });
