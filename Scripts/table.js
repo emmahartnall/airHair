e1bb8595c103305331928617e30128680b7e77a6
Emma Hartnall | Hall <emmahartnall@gmail.com>
2024-10-24 16:44:30 +1100

    separate javaScript two files

--- Scripts/table.js
+++ Scripts/table.js
@@ -1,8 +1,6 @@
-  var latitude;
-  var longitude;
+
 
   document.addEventListener('DOMContentLoaded', function() {
-    findHairdressers();  
 
     // Set the minimum date for the date picker to today
     var today = new Date();
@@ -10,60 +8,7 @@
     document.getElementById('datePicker').value = today;
   });
   
-  function findHairdressers(){
-
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition, showError);
-    } else {
-      alert("Geolocation is not supported by this browser.");
-    }
-  }
-
-  function showPosition(position) {
-    latitude = position.coords.latitude;
-    longitude = position.coords.longitude;
-    
-    const map = new google.maps.Map(document.createElement('div'), {
-      center: { lat: latitude, lng: longitude }, 
-      zoom: 15
-    });
-
-    // Set up the Places service
-    const service = new google.maps.places.PlacesService(map);
-
-    // Define the request
-    const request = {
-        location: map.getCenter(),
-        radius: '1000',
-        type: ['hair_care']
-    };
-
-    // Perform the search
-    service.nearbySearch(request, function(results, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-            createTable(results);
-        } else {
-            console.error('Error with Places service:', status);
-        }
-    });
-  }
-  
-  function showError(error) {
-    switch(error.code) {
-      case error.PERMISSION_DENIED:
-        alert("User denied the request for Geolocation.");
-        break;
-      case error.POSITION_UNAVAILABLE:
-        alert("Location information is unavailable.");
-        break;
-      case error.TIMEOUT:
-        alert("The request to get user location timed out.");
-        break;
-      case error.UNKNOWN_ERROR:
-        alert("An unknown error occurred.");
-        break;
-    }
-  }
+ 
 
   function createTable(results){
     const tableContainer = document.getElementById('tableContainer');
