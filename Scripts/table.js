624c1d5dd4aee67d92fba42c1080eed600a97460
Emma Hartnall | Hall <emmahartnall@gmail.com>
2024-10-24 17:48:49 +1100

    header simulate

--- Scripts/table.js
+++ Scripts/table.js
@@ -1,10 +1,4 @@
-  document.addEventListener('DOMContentLoaded', function() {
-
-    // Set the minimum date for the date picker to today
-    var today = new Date();
-    today = new Date().toISOString().split('T')[0];
-    document.getElementById('datePicker').value = today;
-  });
+  
   
   function createTable(results){
     const tableContainer = document.getElementById('tableContainer');
