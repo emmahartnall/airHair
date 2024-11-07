85ed405bad3a858578e08883713679a58cffb851
Emma Hartnall | Hall <emmahartnall@gmail.com>
2024-11-07 16:48:28 +1100

    Add login functionality

--- Scripts/login.js
+++ Scripts/login.js
@@ -6,6 +6,26 @@ const loggedIn = sessionStorage.getItem("loggedIn");
 
 
 
+function checkIfLoggedIn(){
+    if (loggedIn === "true"){
+        alert("You are already logged in");
+    } else {
+        window.location.href = "Login.html"; 
+    }
+}
+
+
+
+
+
+// Storing an object
+const myObject = { name: 'Alice', age: 25 };
+localStorage.setItem('myObject', JSON.stringify(myObject));
+
+// Retrieving the object
+const retrievedObject = JSON.parse(localStorage.getItem('myObject'));
+console.log(retrievedObject.name); // Outputs: Alice
+
 
 
 // Set data
@@ -13,11 +33,3 @@ localStorage.setItem("sharedVariable", "someValue");
 
 // Get data (on another page or later time)
 const sharedVariable = localStorage.getItem("sharedVariable");
-
-function checkIfLoggedIn(){
-    if (loggedIn === "true"){
-        alert("You are already logged in");
-    } else {
-        window.location.href = "login.html"; 
-    }
-}
