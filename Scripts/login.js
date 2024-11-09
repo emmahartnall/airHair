f53ea2296722d0368632402038f910f12b348194
Emma Hartnall | Hall <emmahartnall@gmail.com>
2024-11-09 15:57:12 +1100

    add login functionality

--- Scripts/login.js
+++ Scripts/login.js
@@ -1,22 +1,44 @@
 // Set data
 sessionStorage.setItem("loggedIn", "false");
 
-// Get data
-const loggedIn = sessionStorage.getItem("loggedIn");
-
-
-
 function checkIfLoggedIn(){
-    if (loggedIn === "true"){
-        alert("You are already logged in");
-    } else {
+    const loggedIn = sessionStorage.getItem("loggedIn");
+    if (loggedIn === "false"){
         window.location.href = "Login.html"; 
     }
 }
 
+function getUserName(){
+    const username = sessionStorage.getItem("username");
+    if(username){
+        return username;
+    } else {
+        return null;
+    }
+}
 
+function login(){
+    usernameElement = document.getElementById("userName");
+    passwordElement = document.getElementById("password");
+    if(!usernameElement || !passwordElement){
+        alert("Username or password not found");
+        return;
+    } else{
+        const username = usernameElement.value;
+        const password = passwordElement.value;
+        sessionStorage.setItem("loggedIn", "true");
+        sessionStorage.setItem("username", username);
+        sessionStorage.setItem("password", password);
+        alert("Login successful");
+        window.location.href = "./home.html";
+    }
+}
 
+function logout(){
+    sessionStorage.setItem("loggedIn", "false");
 
+    window.location.href = "index.html";
+}
 
 // Storing an object
 const myObject = { name: 'Alice', age: 25 };
@@ -25,8 +47,6 @@ localStorage.setItem('myObject', JSON.stringify(myObject));
 // Retrieving the object
 const retrievedObject = JSON.parse(localStorage.getItem('myObject'));
 console.log(retrievedObject.name); // Outputs: Alice
-
-
 
 // Set data
 localStorage.setItem("sharedVariable", "someValue");
